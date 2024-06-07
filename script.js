c14e2b7e7777e329a4a65f44c2f29fe17cbff421
Nimue <s.n.ruschell@gmail.com>
2024-06-07 12:23:59 -0400

    Display results after 5 games to declare winner

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const btnScissors = document.getElementById("scissors");
 const results = document.getElementById("results");
 const playerScore = document.getElementById("pScore");
 const cpuScore = document.getElementById("cScore");
+const h1 = document.getElementById("h1");
 
 function getComputerChoice(){
     let choice = Math.random();
@@ -39,45 +40,70 @@ function getHumanChoice(){
 }
 
 function playRound(humChoice, compChoice){
-   
     const choices = document.createElement("p");
     const resultsTxt = document.createElement("p");
     compChoice = getComputerChoice();
-
-    results.appendChild(choices);
-    choices.textContent = "Player selected " + humChoice + " and Computer selected " + compChoice + ".";
-
-    if(humChoice === compChoice){
-        results.appendChild(resultsTxt);
-        resultsTxt.textContent = "Draw! Try again!";
-    }else if(humChoice == "rock" && compChoice == "scissors"){
-        results.appendChild(resultsTxt);
-        resultsTxt.textContent = "You win! Rock beats scissors!";
-        
-   } else if (humChoice == "scissors" && compChoice == "paper"){
-        results.appendChild(resultsTxt);
-        resultsTxt.textContent = "You win! Scissors beats paper!";
-   } else if (humChoice == "paper" && compChoice == "rock"){
-        results.appendChild(resultsTxt);
-        resultsTxt.textContent = "You win! Paper beats rock!";
-   }else {
-        results.appendChild(resultsTxt);
-        resultsTxt.textContent = "You lose! " + compChoice + " beats " + humChoice + "! :( Try again!";
-   }
-}
-
-getHumanChoice();
-
-function playGame(score){
-    let humanScore = 0;
-    let computerScore = 0;
+    const end = document.createElement("h2");
 
     let pValue = Number(playerScore.innerText);
     let cValue = Number(cpuScore.innerText);
     let newPValue = 0;
     let newCValue = 0;
 
-    for(let i = 0; i <5; i++){
+    results.appendChild(choices);
+    choices.textContent = "Player selected " + humChoice + " and Computer selected " + compChoice + ".";
+
+        if(humChoice === compChoice){
+            results.appendChild(resultsTxt);
+            resultsTxt.textContent = "Draw! Try again!";
+            
+        }else if(humChoice == "rock" && compChoice == "scissors"){
+            results.appendChild(resultsTxt);
+            resultsTxt.textContent = "You win! Rock beats scissors!";
+            newPValue = pValue + 1;
+            playerScore.innerText = newPValue.toString();
+    } else if (humChoice == "scissors" && compChoice == "paper"){
+            results.appendChild(resultsTxt);
+            resultsTxt.textContent = "You win! Scissors beats paper!";
+            newPValue = pValue + 1;
+            playerScore.innerText = newPValue.toString();
+    } else if (humChoice == "paper" && compChoice == "rock"){
+            results.appendChild(resultsTxt);
+            resultsTxt.textContent = "You win! Paper beats rock!";
+            newPValue = pValue + 1;
+            playerScore.innerText = newPValue.toString();
+    }else {
+            results.appendChild(resultsTxt);
+            resultsTxt.textContent = "You lose! " + compChoice + " beats " + humChoice + "! :( Try again!";
+            newCValue = cValue + 1;
+            cpuScore.innerText = newCValue.toString();
+    }
+
+    if (newPValue == 5 || newCValue == 5){
+        if (newPValue > newCValue){
+            h1.appendChild(end);
+            end.textContent = "Congrats! You won!"
+        } else if (newCValue > newPValue){
+            h1.appendChild(end);
+            end.textContent = "Sorry, you lost! :("
+        }
+    }
+
+}
+
+getHumanChoice();
+
+
+/*function playGame(score){
+   // let humanScore = 0;
+    //let computerScore = 0;
+    const end = document.createElement("h2");
+
+    let pValue = Number(playerScore.innerText);
+    let cValue = Number(cpuScore.innerText);
+    let newPValue = 0;
+    let newCValue = 0;
+//for(let i = 0; i <5; i++){
         let win = playRound();
         if (win == true){
             newPValue = pValue + 1;
@@ -86,5 +112,15 @@ function playGame(score){
             newCValue = cValue + 1;
             cpuScore.innerText = newCValue.toString();
         }
-    }       
-}
+
+        if (newPValue == 5 || newCValue == 5){
+            if (newPValue > newCValue){
+                h1.appendChild(end);
+                end.textContent = "Congrats! You won!"
+            } else if (newCValue > newPValue){
+                h1.appendChild(end);
+                end.textContent = "Sorry, you lost! :("
+            }
+        }
+    //}       
+}*/
